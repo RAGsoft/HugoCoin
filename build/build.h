c9f2ca065802cdf9c46bd79dce6baa996d7d4eb4
root <root@raglinux.ragsoft.com.au>
2013-11-05 22:29:23 +1030

    changes

---
+++ build/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2013-11-03 21:36:02 +1030"
--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #foocoin2TEST3\r");
-            Send(hSocket, "WHO #foocoin2TEST3\r");
+            Send(hSocket, "JOIN #HugoCoin2TEST3\r");
+            Send(hSocket, "WHO #HugoCoin2TEST3\r");
         } else {
-            // randomly join #foocoin00-#foocoin99
+            // randomly join #HugoCoin00-#HugoCoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #foocoin2%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #foocoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #HugoCoin2%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #HugoCoin2%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
